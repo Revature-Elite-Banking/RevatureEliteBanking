7680bb6d2fafbfe5eb200fb6a7bf7b8ca77cab67
Rey Blackburn <rey.blackburn@revature.net>
2021-11-01 14:50:16 -0600

    Started Spring Starter Project with Gateway, PostgreSQL Driver, Spring Boot Actuator, Spring Boot DevTools, SPring Data JPA, Spring Web

---
+++ RevatureEliteBankingBackend/src/main/java/com/revature/RevatureEliteBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevatureEliteBankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevatureEliteBankingBackendApplication.class, args);
+	}
+
+}
