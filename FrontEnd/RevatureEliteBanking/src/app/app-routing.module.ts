d2c0824a788d59e28b49905d22fcc9ffa7c88c34
bgarber0343 <bgarber0343@gmail.com>
2021-11-02 16:50:58 -0500

    built login/registration pages and added functionality for login service

--- FrontEnd/RevatureEliteBanking/src/app/app-routing.module.ts
+++ FrontEnd/RevatureEliteBanking/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"",
+    component:LoginComponent
+  },
+
+  {
+    path:"login",
+    component:LoginComponent
+  },
+
+  {
+    path:"registration",
+    component:RegistrationComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
