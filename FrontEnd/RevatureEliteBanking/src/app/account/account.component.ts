f52758f7f1cd50d2dc63e1a4331d65aa356e0dad
Andre Montenegro <ajmc200@gmail.com>
2021-11-04 13:35:23 -0400

    accounts display

--- FrontEnd/RevatureEliteBanking/src/app/account/account.component.ts
+++ FrontEnd/RevatureEliteBanking/src/app/account/account.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IAccounts } from '../accounts';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  // placeholder info
+  name = ''
+  username = ''
+  chips = 10
+  public accounts!: IAccounts; 
+
+  constructor(
+    private http:HttpClient,
+    private accountsService:AccountsService
+  ) { 
+  }
 
   ngOnInit(): void {
+    this.accountsService.getAccounts()
+      .subscribe(data => this.accountView(data))
+  }
+
+  accountView(accountsInfo:IAccounts){
+    this.name = accountsInfo.name
+    this.username = accountsInfo.username
+    this.chips = accountsInfo.chipCount
+    
   }
 
 }
--- FrontEnd/RevatureEliteBanking/src/app/app-routing.module.ts
+++ FrontEnd/RevatureEliteBanking/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
@@ -18,6 +19,11 @@ const routes: Routes = [
   {
     path:"registration",
     component:RegistrationComponent
+  },
+
+  {
+    path:"account",
+    component:AccountComponent
   }
 
 ];
