d4b8498b7ae130b8bf2d0f7b820686fb0fbe41ac
Rey Blackburn <rey.blackburn@revature.net>
2021-11-04 18:14:13 -0600

    Add my security settings to test for cors issues

--- RevatureEliteBankingBackend/src/main/java/com/revature/controllers/LoginController.java
+++ RevatureEliteBankingBackend/src/main/java/com/revature/controllers/LoginController.java
@@ -1,64 +1,46 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.models.AuthRequest;
+import com.revature.models.LoginDTO;
+import com.revature.services.LoginService;
 import com.revature.utils.JwtUtil;
 
 @RestController
-@RequestMapping(value="/login")
-@CrossOrigin
+@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials ="true")
 public class LoginController {
 	
-	@Autowired
-	private JwtUtil jwtUtil;
+	private LoginService lService;
 	
 	@Autowired
-	private AuthenticationManager authenticationManager;
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
-	
-	@GetMapping
-	public ResponseEntity success() {
-		return ResponseEntity.ok("successfully done");
+	public LoginController(LoginService ls) {
+		super();
+		this.lService = ls;
 	}
 	
-	@PostMapping(value = "/validate")
-	public ResponseEntity<?> generateToken(@RequestBody AuthRequest authrequest) throws Exception {
-		try {
-			authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(authrequest.getUsername(),authrequest.getPassword() )
-			);
-		} catch (BadCredentialsException e) {
-
-			return ResponseEntity
-		            .status(HttpStatus.FORBIDDEN)
-		            .body("Error Message");
+	@PostMapping
+	public ResponseEntity<LoginDTO> checkCredentials(@RequestBody LoginDTO ldto){
+		
+		if(lService.checkCredentials(ldto.getUsername(), ldto.getPassword())) {
+			//Give JWT for the user
+			String token = JwtUtil.getJWTToken(ldto.getUsername());
+			ldto.setToken(token);
+			
+			//Send JWT back to the client
+			return ResponseEntity.ok().body(ldto);
 		}
 		
-//		final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-		
-		String jwt =  jwtUtil.generateToken(authrequest.getUsername());
-		return ResponseEntity.ok(jwt);
-//		return (ResponseEntity) ResponseEntity.ok().header("jwt", "100");
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		
 	}
+
 }
--- RevatureEliteBankingBackend/src/main/java/com/revature/security/JWTAuthorizationFilter.java
+++ RevatureEliteBankingBackend/src/main/java/com/revature/security/JWTAuthorizationFilter.java
@@ -1,58 +1,77 @@
 package com.revature.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.revature.utils.JwtUtil;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtUtil jwtUtil;
-    @Autowired
-    private UserDetailsService service;
+	private final String HEADER = "Authorization";
+	private final String PREFIX = "Bearer ";
+	private final String SECRET = "mySecretKey";
 
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+		try {
+			if (checkJWTToken(request, response)) {
+				Claims claims = validateToken(request);
+				if (claims.get("authorities") != null) {
+					setUpSpringAuthentication(claims);
+				} else {
+					SecurityContextHolder.clearContext();
+				}
+			}else {
+				SecurityContextHolder.clearContext();
+			}
+			chain.doFilter(request, response);
+		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+			return;
+		}
+	}	
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+	private Claims validateToken(HttpServletRequest request) {
+		String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
+		return Jwts.parser().setSigningKey(SECRET.getBytes()).parseClaimsJws(jwtToken).getBody();
+	}
 
-        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+	/**
+	 * Authentication method in Spring flow
+	 * 
+	 * @param claims
+	 */
+	private void setUpSpringAuthentication(Claims claims) {
+		@SuppressWarnings("unchecked")
+		List<String> authorities = (List) claims.get("authorities");
 
-        String token = null;
-        String userName = null;
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null,
+				authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+		SecurityContextHolder.getContext().setAuthentication(auth);
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            token = authorizationHeader.substring(7);
-            userName = jwtUtil.extractUsername(token);
-        }
+	}
 
-        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+	private boolean checkJWTToken(HttpServletRequest request, HttpServletResponse res) {
+		String authenticationHeader = request.getHeader(HEADER);
+		if (authenticationHeader == null || !authenticationHeader.startsWith(PREFIX))
+			return false;
+		return true;
+	}
 
-            UserDetails userDetails = service.loadUserByUsername(userName);
-
-            if (jwtUtil.validateToken(token, userDetails)) {
-
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            }
-        }
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
-    }
 }
