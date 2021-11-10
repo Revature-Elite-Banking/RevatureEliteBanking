98f2ab76b2cadf3d35c8f2b4d5c9a221e6b4d2d0
Mesfin Tabor <mesfinm.tabor@yahoo.com>
2021-11-10 11:52:44 -0500

    Notification date format updated

--- FrontEnd/RevatureEliteBanking/src/app/account/account.component.ts
+++ FrontEnd/RevatureEliteBanking/src/app/account/account.component.ts
@@ -7,6 +7,7 @@ import { AccountsService } from '../accounts.service';
 import { ACCOUNTS } from '../mock-accounts';
 import { AccountIdService } from '../services/account-id.service';
 import * as alertyfy from 'alertifyjs';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-account',
@@ -72,8 +73,7 @@ export class AccountComponent implements OnInit {
     this.http.post(url,trans,{responseType: 'text'}).subscribe(r=>{});
     console.log(trans);
 
-    let date: Date = new Date(); 
     alertyfy.set('notifier','position', 'top-right');
-    var notification = alertyfy.notify(date +" Hello "+localStorage.getItem('username') +" $"+this.amount+" is transfered from account " + this.from +" to "+this.to, 'success', 30, function(){  console.log('dismissed'); });
+    var notification = alertyfy.notify(formatDate(Date.now(),'EEE, dd MMM YYYY hh:mm:ss','en-US') +" Hello "+localStorage.getItem('username') +" $"+this.amount+" is transfered from account " + this.from +" to "+this.to, 'success', 30, function(){  console.log('dismissed'); });
   }
 }
